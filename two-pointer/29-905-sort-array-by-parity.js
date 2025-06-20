0d5ba83ef3fe54761e8a1e7714f8af1674987973
Jatin Dhamija <jd.codebase@gmail.com>
2025-06-20 12:04:49 +0530

    29-905-sort-array-by-parity

---
+++ two-pointer/29-905-sort-array-by-parity.js
@@ -0,0 +1,18 @@
+var sortArrayByParity = function (nums) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left < right) {
+    if (nums[left] % 2 > nums[right] % 2) {
+      [nums[left], nums[right]] = [nums[right], nums[left]];
+    }
+
+    if (nums[left] % 2 === 0) left++;
+    if (nums[right] % 2 === 1) right--;
+  }
+
+  return nums;
+};
+
+console.log(sortArrayByParity([3, 1, 2, 4]));
+console.log(sortArrayByParity([0]));
