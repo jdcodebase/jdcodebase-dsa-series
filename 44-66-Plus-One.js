3b5edb1babce50d7a53dab50b14af51b1945c588
jatin dhamija <jd.codebase@gmail.com>
2025-08-12 15:21:07 +0530

    44-66-Plus-One

---
+++ 44-66-Plus-One.js
@@ -0,0 +1,16 @@
+var plusOne = function (digits) {
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (digits[i] < 9) {
+      digits[i]++;
+      return digits;
+    }
+    digits[i] = 0;
+  }
+
+  digits.unshift(1);
+  return digits;
+};
+
+console.log(plusOne([1, 2, 3])); // Output: [1, 2, 4]
+console.log(plusOne([4, 3, 2, 1])); //  Output: [4, 3, 2, 2]
+console.log(plusOne([9])); //  Output: [1, 0]
